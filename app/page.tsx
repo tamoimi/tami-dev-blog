9cd483ad3bbd1f8211a0e0337aaa04a6c46f0746
tami <xorud7979@gmail.com>
2023-09-15 17:51:04 +0900

    feat: create postcard

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,36 @@
+import PostCard from "components/PostCard";
+import { Post, allPosts } from "contentlayer/generated";
+import { compareDesc, format, parseISO } from "date-fns";
 import Link from "next/link";
-import Navigation from "../components/Nav";
 
 export default function Home() {
+  const posts = allPosts.sort((a, b) =>
+    compareDesc(new Date(a.date), new Date(b.date))
+  );
   return (
     <div className="container ">
       {/* introduction */}
-      <main>
+      <main className="my-10">
         <h1 className="font-bold mb-3">Tami Kim</h1>
         <h2 className="my-5">
-          I&#39;m a Front End developer based in South Korea. I&#39;m interested
-          in dev rel, design systems, web3, user/dev experience and under
-          engineering.
+          I&#39;m a Front End developer based in South Korea. I&#39;m passion
+          for creating visually stunning and user-friendly websites.
         </h2>
         <h2>
-          I&#39;m currently working at OP as a Front End developer to grow its
-          payment solution.
+          I&#39;m currently working at Optatum Platform as a Front End developer
+          to grow its payment solution.
         </h2>
-        <h2></h2>
       </main>
-
-      {/* post section*/}
-      <div className="mx-auto"></div>
-
+      <div className="my-10">
+        <h1 className="font-bold mb-5">Posts</h1>
+        <article>
+          <div className="mx-auto">
+            {posts.map((post, idx) => (
+              <PostCard key={idx} {...post} />
+            ))}
+          </div>
+        </article>
+      </div>
       {/* footer section or not will see */}
       <div className="mx-auto"></div>
     </div>
