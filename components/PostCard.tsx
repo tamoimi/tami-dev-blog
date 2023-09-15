0e2d8db3fa477b781980560cba146386fb80593c
tami <xorud7979@gmail.com>
2023-09-15 17:51:42 +0900

    feat: create post card component for home page

---
+++ components/PostCard.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import { format, parseISO } from "date-fns";
+import { Post } from "contentlayer/generated";
+
+const PostCard = (post: Post) => {
+  return (
+    <>
+      <div className="mb-8">
+        <h2 className="mb-1 text-xl">
+          <Link
+            href={post.url}
+            className=" hover:text-blue-900 dark:text-blue-400"
+          >
+            {post.title}
+          </Link>
+        </h2>
+        <time dateTime={post.date} className="mb-2 block text-xs text-gray-600">
+          {format(parseISO(post.date), "LLLL d, yyyy")}
+        </time>
+        {/* <div
+          className="text-sm [&>*]:mb-3 [&>*:last-child]:mb-0"
+          dangerouslySetInnerHTML={{ __html: post.body.html }}
+        /> */}
+      </div>
+    </>
+  );
+};
+export default PostCard;
