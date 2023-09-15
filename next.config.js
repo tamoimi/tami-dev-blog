5d4acb84da6cb8e2935e35270912313eee327a77
tami <xorud7979@gmail.com>
2023-09-15 15:18:54 +0900

    feat: add contentlayer for next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
+// Contentlayer를 다음 개발 및 빌드 프로세스에 연결하려면 withContentlayer 메서드를 사용하여 Next.js 구성을 래핑하는 것이 좋다.
+const {withContentlayer} = require('next-contentlayer')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = withContentlayer(nextConfig)
