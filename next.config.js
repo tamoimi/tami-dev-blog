e3d6ea3ac9293216492ae5b3a6b7af9cc1704c5f
tami <xorud7979@gmail.com>
2023-09-15 17:02:21 +0900

    refactor: refactor

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 // Contentlayer를 다음 개발 및 빌드 프로세스에 연결하려면 withContentlayer 메서드를 사용하여 Next.js 구성을 래핑하는 것이 좋다.
-const {withContentlayer} = require('next-contentlayer')
+const { withContentlayer } = require("next-contentlayer");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = withContentlayer(nextConfig)
+module.exports = withContentlayer(nextConfig);
