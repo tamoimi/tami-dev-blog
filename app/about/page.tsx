e0d8404a9738284b317a57de53fd44db4b0f89b8
tami <xorud7979@gmail.com>
2023-09-15 15:19:43 +0900

    style: style

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,19 @@
 const About = () => {
   return (
-    <div>about</div>
-  )
-}
-export default About
+    <div>
+      I am trying to create my new blog 🦦 Please wait a moment :)
+      <iframe
+        src="https://giphy.com/embed/Yggr0uQUbA79C"
+        width="100%"
+        className="giphy-embed"
+        allowFullScreen
+        style={{ margin: "20px 0px 20px 0px" }}
+      ></iframe>
+      <p>
+        Front End developer, living in everywhere <br /> I will meet you very
+        soon!
+      </p>
+    </div>
+  );
+};
+export default About;
