013c9c40cd133db4ee475aadea3430b39d342b15
tami <tamiortami@gmail.com>
2023-09-22 16:43:12 +0900

    test: test for mdx

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const { withContentlayer } = require("next-contentlayer");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = { reactStrictMode: true, swcMinify: true };
 
 module.exports = withContentlayer(nextConfig);
+
