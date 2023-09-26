86aa3e73ec8f56b7a04969af4a746a5f319a7b25
tami <tamiortami@gmail.com>
2023-09-26 17:34:20 +0900

    feat: created quote page

--- app/post/page.tsx
+++ app/post/page.tsx
@@ -1,9 +1,19 @@
-const Post = () => {
-    return (
-      <div className="my-10">
-       post
-      </div>
-    );
-  };
-  export default Post;
-  
+import PostCard from "components/PostCard";
+import { allPosts } from "contentlayer/generated";
+import { compareDesc } from "date-fns";
+
+const Posts = () => {
+  const posts = allPosts.sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)));
+  return (
+    <div className="my-10">
+      <article>
+        <div className="mx-auto">
+          {posts.map((post, idx) => (
+            <PostCard key={idx} {...post} />
+          ))}
+        </div>
+      </article>
+    </div>
+  );
+};
+export default Posts;
