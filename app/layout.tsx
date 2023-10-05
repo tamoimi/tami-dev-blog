99005e8d1528258c3b58a1bd0207bff6fc004369
tami <tamiortami@gmail.com>
2023-10-05 17:43:10 +0900

    error: fixed console error

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,26 +46,20 @@ export default function RootLayout({ children }: RootLayoutProps) {
                 {/* mode toggle */}
                 <ModeToggle />
                 <nav className="ml-auto text-sm font-medium space-x-6 flex items-center justify-between">
-                  <Link href="/">
-                    Home
-                  </Link>
-                  <Link href="/about">
-                    About
-                  </Link>
-                  <Link href="/post">
-                    Post
-                  </Link>
+                  <Link href="/">Home</Link>
+                  <Link href="/about">About</Link>
+                  <Link href="/post">Post</Link>
                   <Link href="/quote">
                     <svg
                       className="h-5 w-5"
                       width="24"
                       height="24"
                       viewBox="0 0 24 24"
-                      stroke-width="2"
+                      strokeWidth="1"
                       stroke="currentColor"
                       fill="none"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                     >
                       <path stroke="none" d="M0 0h24v24H0z" /> <path d="M3 20l1.3 -3.9a9 8 0 1 1 3.4 2.9l-4.7 1" />
                       <line x1="12" y1="12" x2="12" y2="12.01" /> <line x1="8" y1="12" x2="8" y2="12.01" />
