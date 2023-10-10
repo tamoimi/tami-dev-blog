2d9e5f95a931d8597fdcb09f05602d64eec8904a
tami <tamiortami@gmail.com>
2023-10-10 17:29:16 +0900

    feat: created Comment component

---
+++ components/Comment.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useTheme } from "next-themes";
+
+const Comment = () => {
+  const { setTheme, theme } = useTheme();
+
+  const toggleTheme = () => {
+    const newTheme = theme === "dark" ? "github-dark" : "github-light";
+    setTheme(newTheme);
+  };
+
+  return (
+    <section
+      ref={(elem) => {
+        if (!elem) {
+          return;
+        }
+        const scriptElem = document.createElement("script");
+        scriptElem.src = "https://utteranc.es/client.js";
+        scriptElem.async = true;
+        scriptElem.setAttribute("repo", "tamoimi/tami-dev-blog");
+        scriptElem.setAttribute("issue-term", "title");
+        scriptElem.setAttribute("theme", theme === "dark" ? "github-dark" : "github-light");
+        scriptElem.setAttribute("label", "blog-comment");
+        scriptElem.crossOrigin = "anonymous";
+        elem.replaceChildren(scriptElem);
+      }}
+    />
+  );
+};
+
+export default Comment;
