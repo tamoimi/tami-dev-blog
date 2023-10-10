87b8cc08fce2e1fb1b8028a5cae9a21057df1e5c
tami <tamiortami@gmail.com>
2023-10-10 17:28:54 +0900

    style: changed button bg

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -10,10 +10,10 @@ const About = () => {
           Front End developer, living in everywhere 🌎 <br /> I will meet you very soon!
         </p>
         <div className="flex space-x-2 mt-6">
-          <button type="button" className="bg-teal-600 text-teal-50 p-1 rounded-md hover:bg-teal-500 transition-all">
+          <button type="button" className="bg-cyan-600 text-teal-50 p-1 rounded-md hover:bg-teal-500 transition-all">
             <a href="https://github.com/tamoimi">GitHub</a>
           </button>
-          <button className="bg-teal-600 text-teal-50 p-1 rounded-md  hover:bg-teal-500 transition-all">
+          <button className="bg-cyan-600 text-teal-50 p-1 rounded-md  hover:bg-teal-500 transition-all">
             <a href="https://www.linkedin.com/in/tamiortami/">LinkedIn</a>
           </button>
         </div>
