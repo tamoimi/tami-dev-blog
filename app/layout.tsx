ccc68dd0bcb1ecccba69164c62e9387769cd71d9
tami <tamiortami@gmail.com>
2023-10-16 14:32:32 +0900

    remove: study page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,9 @@
+import { ModeToggle } from "components/ModeToggle";
+import { ThemeProvider } from "components/ThemeProvider";
 import type { Metadata } from "next";
 import { Inter, Noto_Sans_KR, Roboto } from "next/font/google";
 import Link from "next/link";
 import "./globals.css";
-import { ModeToggle } from "components/ModeToggle";
-import { ThemeProvider } from "components/ThemeProvider";
-import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -50,25 +49,6 @@ export default function RootLayout({ children }: RootLayoutProps) {
                   <Link href="/">Home</Link>
                   <Link href="/about">About</Link>
                   <Link href="/post">Post</Link>
-                  <Link href="/study">
-                    
-                    <svg
-                      className="h-5 w-5"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      strokeWidth="1"
-                      stroke="currentColor"
-                      fill="none"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    >
-                      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                      <path d="M6 4h11a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-11a1 1 0 0 1 -1 -1v-14a1 1 0 0 1 1 -1m3 0v18"></path>
-                      <path d="M13 8l2 0"></path>
-                      <path d="M13 12l2 0"></path>
-                    </svg>
-                  </Link>
                   <Link href="/quote">
                     <svg
                       className="h-5 w-5"
