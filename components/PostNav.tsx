9c162d4715b803e1af8e2fbf5386be57180afb52
tami <tamiortami@gmail.com>
2023-10-16 14:34:00 +0900

    feat: created postNav tap as tags

---
+++ components/PostNav.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const PostNav = () => {
+  const router = usePathname();
+
+  return (
+    <ul className="flex content-center text-xs font-medium text-center text-gray-500 dark:text-gray-400 mb-5">
+      <li className="mr-2">
+        <Link
+          href="/post"
+          passHref
+          className={`inline-block px-4 py-3 ${
+            router === "/post" ? "bg-gray-100 dark:bg-gray-800" : ""
+          } rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white`}
+        >
+          Post
+        </Link>
+      </li>
+      <li className="mr-2">
+        <Link
+          href="/javascript"
+          passHref
+          className={`inline-block px-4 py-3 ${
+            router === "/javascript" ? "bg-gray-100 dark:bg-gray-800" : ""
+          } rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white`}
+        >
+          JavaScript
+        </Link>
+      </li>
+      <li className="mr-2">
+        <Link
+          href="/algorithm"
+          passHref
+          className={`inline-block px-4 py-3  ${
+            router === "/algorithm" ? "bg-gray-100 dark:bg-gray-800" : ""
+          } rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white`}
+        >
+          Algorithm
+        </Link>
+      </li>
+      <li className="mr-2">
+        <Link
+          href="/library"
+          passHref
+          className={`inline-block px-4 py-3 ${
+            router === "/library" ? "bg-gray-100 dark:bg-gray-800" : ""
+          } rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white`}
+        >
+          Library
+        </Link>
+      </li>
+    </ul>
+  );
+};
+
+export default PostNav;
