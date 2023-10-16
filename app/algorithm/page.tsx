56134d41ad81c319e694632b2dddda941de333ed
tami <tamiortami@gmail.com>
2023-10-16 14:32:40 +0900

    feat: algorithm page

---
+++ app/algorithm/page.tsx
@@ -0,0 +1,21 @@
+import PostNav from "components/PostNav";
+import AlgorithmCard from "components/StudyCard";
+import { allAlgorithms } from "contentlayer/generated";
+import { compareDesc } from "date-fns";
+
+const Algorithms = () => {
+  const algorithms = allAlgorithms.sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)));
+  return (
+    <div className="my-10">
+      <PostNav />
+      <article>
+        <div className="mx-auto">
+          {algorithms.map((algorithm, idx) => (
+            <AlgorithmCard key={idx} {...algorithm} />
+          ))}
+        </div>
+      </article>
+    </div>
+  );
+};
+export default Algorithms;
