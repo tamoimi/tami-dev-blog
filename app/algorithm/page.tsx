c4b75097e31f59a8657acbda60fbacf460ae0d45
tami <tamiortami@gmail.com>
2023-10-16 14:34:09 +0900

    updated

--- app/algorithm/page.tsx
+++ app/algorithm/page.tsx
@@ -1,5 +1,5 @@
 import PostNav from "components/PostNav";
-import AlgorithmCard from "components/StudyCard";
+import AlgorithmCard from "components/AlgorithmCard";
 import { allAlgorithms } from "contentlayer/generated";
 import { compareDesc } from "date-fns";
 
