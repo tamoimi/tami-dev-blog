1f184b9b7930494f78b5ca7d3ba11da62b5cea3b
tami <tamiortami@gmail.com>
2023-10-19 16:37:48 +0900

    feat: transition for bg

--- components/AlgorithmCard.tsx
+++ components/AlgorithmCard.tsx
@@ -5,20 +5,18 @@ import Link from "next/link";
 const AlgorithmCard = (algorithm: Algorithm) => {
   return (
     <>
-      <div className="mb-8">
-        <h2 className="mb-1 text-xl">
-          <Link href={algorithm.slug} className=" hover:text-cyan-600 dark:hover:text-gray-200 ">
-            {algorithm.title}
-          </Link>
-        </h2>
-        <p className="mb-1 text-stone-600 dark:text-stone-300 text-sm">{algorithm.description}</p>
-        <time dateTime={algorithm.date} className="mb-2 block text-xs text-gray-600 dark:text-gray-400">
-          {format(parseISO(algorithm.date), "LLLL d, yyyy")}
-        </time>
-        {/* <div
+      <div className="p-2 rounded-md hover:bg-gray-100 transition-colors duration-300 ease-in-out dark:hover:bg-gray-800">
+        <Link href={algorithm.slug}>
+          <h2 className="mb-1 text-xl">{algorithm.title}</h2>
+          <p className="mb-1 text-stone-600 dark:text-stone-300 text-sm">{algorithm.description}</p>
+          <time dateTime={algorithm.date} className="mb-2 block text-xs text-gray-600 dark:text-gray-400">
+            {format(parseISO(algorithm.date), "LLLL d, yyyy")}
+          </time>
+          {/* <div
           className="text-sm [&>*]:mb-3 [&>*:last-child]:mb-0"
           dangerouslySetInnerHTML={{ __html: post.body.html }}
         /> */}
+        </Link>
       </div>
     </>
   );
