b80ac2069ac9b48ce29dc4305417de44db22cd47
tami <tamiortami@gmail.com>
2023-10-30 17:10:37 +0900

    style: gif file

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,12 +1,16 @@
+import Image from "next/image";
+
 const About = () => {
   return (
     <div className="my-10 text-base">
       {/* add gif */}
       <div className="m-auto">
-        <p className="mb-5">
+        {/* <Image src="/giphy.gif" alt="Computer man" width={0} height={0} sizes="100vw"/> */}
+        <Image src="/giphy.gif" alt="Computer man" width={500} height={300} />
+        {/* <p className="my-5">
           I am still working on my new blog 🦦 <br />
-        </p>
-        <p>
+        </p> */}
+        <p className="my-5">
           Front End developer, living in everywhere 🌎 <br /> I will meet you very soon!
         </p>
         <div className="flex space-x-3 mt-6 text-gray-500 dark:text-gray-400 text-sm">
