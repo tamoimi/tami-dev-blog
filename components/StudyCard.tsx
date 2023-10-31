10ac4def19d3955fe86499b946bc3a64f2f32094
tami <tamiortami@gmail.com>
2023-10-31 11:51:46 +0900

    feat: study card component

---
+++ components/StudyCard.tsx
@@ -0,0 +1,24 @@
+import { Study } from "contentlayer/generated";
+import { format, parseISO } from "date-fns";
+import Link from "next/link";
+
+const StudyCard = (study: Study) => {
+  return (
+    <>
+      <div className="p-2 rounded-md hover:bg-gray-100 transition-colors duration-300 ease-in-out dark:hover:bg-gray-800">
+        <Link href={study.slug}>
+          <h2 className="mb-1 text-xl">{study.title}</h2>
+          <p className="mb-1 text-stone-600 dark:text-stone-300 text-sm">{study.description}</p>
+          <time dateTime={study.date} className="mb-2 block text-xs text-gray-600 dark:text-gray-400">
+            {format(parseISO(study.date), "LLLL d, yyyy")}
+          </time>
+          {/* <div
+          className="text-sm [&>*]:mb-3 [&>*:last-child]:mb-0"
+          dangerouslySetInnerHTML={{ __html: post.body.html }}
+        /> */}
+        </Link>
+      </div>
+    </>
+  );
+};
+export default StudyCard;
