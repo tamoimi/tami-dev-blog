e6b4d40894fab1506d62a34f6fb21e34088e4c35
tami <tamiortami@gmail.com>
2024-02-08 16:41:56 +0900

    feat: import navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
-import { ModeToggle } from "components/ModeToggle";
+import NavBar from "components/NavBar";
 import { ThemeProvider } from "components/ThemeProvider";
 import type { Metadata } from "next";
 import { Inter, Noto_Sans_KR, Roboto } from "next/font/google";
-import Link from "next/link";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,7 +19,7 @@ const notoSansKr = Noto_Sans_KR({
 
 export const metadata: Metadata = {
   title: "Tami Kim",
-  description: "Tami Kim Dev Blog",
+  description: "Tami Kim's Dev Blog",
 };
 
 interface RootLayoutProps {
@@ -42,32 +41,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <div className="max-w-2xl mx-auto py-10 px-4 selection:bg-slate-300 dark:selection:bg-slate-800 ">
             <header>
-              <div className="flex items-center justify-between">
-                {/* mode toggle */}
-                <ModeToggle />
-                <nav className="ml-auto text-sm font-medium space-x-6 flex items-center justify-between">
-                  <Link href="/">Home</Link>
-                  <Link href="/about">About</Link>
-                  <Link href="/post">Post</Link>
-                  <Link href="/quote">
-                    <svg
-                      className="h-5 w-5"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      strokeWidth="1"
-                      stroke="currentColor"
-                      fill="none"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    >
-                      <path stroke="none" d="M0 0h24v24H0z" /> <path d="M3 20l1.3 -3.9a9 8 0 1 1 3.4 2.9l-4.7 1" />
-                      <line x1="12" y1="12" x2="12" y2="12.01" /> <line x1="8" y1="12" x2="8" y2="12.01" />
-                      <line x1="16" y1="12" x2="16" y2="12.01" />
-                    </svg>
-                  </Link>
-                </nav>
-              </div>
+              <NavBar />
             </header>
 
             {/* children will be all the pages */}
