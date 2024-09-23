d5c4af4f84f917372d030f748c0b6143a9496df5
tami <tamiortami@gmail.com>
2024-09-23 10:33:17 +0900

    feat: pagination for study page

--- app/study/page.tsx
+++ app/study/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import usePagination from "app/hooks/use-pagination";
 import LeftArrow from "components/Icons/left-arrow";
 import RightArrow from "components/Icons/right-arrow";
+import Pagination from "components/pagination";
 import PostNav from "components/post-nav";
 import StudyCard from "components/study-card";
 import { allStudies } from "contentlayer/generated";
@@ -15,42 +17,19 @@ const Javascript = () => {
   const studies = allStudies.sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)));
 
   // ===================================================================================================================
-  // state
-  // ===================================================================================================================
-  // current page state
-  const [currentPage, setCurrentPage] = useState(1);
-
-  // ===================================================================================================================
   // pagination
   // ===================================================================================================================
-  // 한 페이지에 표시할 데이터 개수
+  // current page size
   const size = 5;
 
-  // 전체 페이지 개수: 데이터를 페이지당 5개로 나웠을 때 남는 데이터를 새로운 페이지로 표시
-  const totalPages = Math.ceil(allStudies.length / size);
-
-  // 현재 페이지에서 마지막 데이터의 인덱스를 계산한다.
-  const indexOfLast = currentPage * size;
-
-  // 현재 페이지에서 첫 번째 데이터의 인덱스를 계산한다.
-  const indexOfFirst = indexOfLast - size;
-
-  // 현재 페이지에서 보여줄 데이터의 배열이다. slice 함수로 studies 배열에서 일부 데이터를 잘라낸다.
-  const currentStudies = studies.slice(indexOfFirst, indexOfLast);
-
-  // currentPage 값이 전체 페이지 수보다 작을 때만 작동
-  const handleNextPage = () => {
-    if (currentPage < totalPages) {
-      setCurrentPage((prevPage) => prevPage + 1);
-    }
-  };
-
-  // currentPage 값이 1보다 클 때만 작동
-  const handlePrevPage = () => {
-    if (currentPage > 1) {
-      setCurrentPage((prevPage) => prevPage - 1);
-    }
-  };
+  // usePagination hook
+  const {
+    currentData: currentStudies,
+    currentPage,
+    totalPages,
+    handleNextPage,
+    handlePrevPage,
+  } = usePagination(studies, size);
 
   return (
     <div className="my-10">
@@ -63,25 +42,12 @@ const Javascript = () => {
         </div>
 
         {/* Pagination Controls */}
-        <div className="pagination-controls flex justify-center items-center gap-3 mt-4">
-          <button
-            onClick={handlePrevPage}
-            disabled={currentPage === 1}
-            className="p-1 mx-2 bg-gray-300 rounded disabled:opacity-50 text-sm"
-          >
-            <LeftArrow />
-          </button>
-          <span className="text-sm">
-            Page {currentPage} of {totalPages}
-          </span>
-          <button
-            onClick={handleNextPage}
-            disabled={currentPage === totalPages}
-            className="p-1 mx-2 bg-gray-300 rounded disabled:opacity-50"
-          >
-            <RightArrow />
-          </button>
-        </div>
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          handleNextPage={handleNextPage}
+          handlePrevPage={handlePrevPage}
+        />
       </article>
     </div>
   );
