55acd8e01c2d80be3d97c44531692a48e576f331
tami <tamiortami@gmail.com>
2024-09-23 09:48:12 +0900

    feat: pagination component

--- components/pagination.tsx
+++ components/pagination.tsx
@@ -1,16 +1,34 @@
+import LeftArrow from "./Icons/left-arrow";
+import RightArrow from "./Icons/right-arrow";
+
 interface PaginationProps {
-    currentPage: number;
-    totalPages: number;
-    handleNextPage: () => void;
-    handlePrevPage: () => void;
+  currentPage: number;
+  totalPages: number;
+  handleNextPage: () => void;
+  handlePrevPage: () => void;
 }
 
-
-const Pagination = () => {
-return (
-    <>
-    
-    </>
-)
-}
-export default Pagination
+const Pagination = ({ currentPage, totalPages, handleNextPage, handlePrevPage }: PaginationProps) => {
+  return (
+    <div className="pagination-controls flex justify-center items-center gap-3 mt-4">
+      <button
+        onClick={handlePrevPage}
+        disabled={currentPage === 1}
+        className="p-1 mx-2 bg-gray-300 rounded disabled:opacity-50 text-sm"
+      >
+        <LeftArrow />
+      </button>
+      <span className="text-sm">
+        Page {currentPage} of {totalPages}
+      </span>
+      <button
+        onClick={handleNextPage}
+        disabled={currentPage === totalPages}
+        className="p-1 mx-2 bg-gray-300 rounded disabled:opacity-50"
+      >
+        <RightArrow />
+      </button>
+    </div>
+  );
+};
+export default Pagination;
