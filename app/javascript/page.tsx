a8cfcfb70f198342640314c39a041805db21fa2d
tami <tamiortami@gmail.com>
2024-09-24 17:59:08 +0900

    feat: pagination for javascript page

--- app/javascript/page.tsx
+++ app/javascript/page.tsx
@@ -1,19 +1,44 @@
+import usePagination from "app/hooks/use-pagination";
 import JavaScriptCard from "components/java-script-card";
+import Pagination from "components/pagination";
 import PostNav from "components/post-nav";
 import { allJavaScripts } from "contentlayer/generated";
 import { compareDesc } from "date-fns";
 
 const Javascript = () => {
   const javaScripts = allJavaScripts.sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)));
+  // ===================================================================================================================
+  // pagination
+  // ===================================================================================================================
+  // current page size
+  const size = 5;
+
+  // usePagination hook
+  const {
+    currentData: currentJavaScripts,
+    currentPage,
+    totalPages,
+    handleNextPage,
+    handlePrevPage,
+  } = usePagination(javaScripts, size);
+
   return (
     <div className="my-10">
       <PostNav />
       <article>
         <div className="mx-auto">
-          {javaScripts.map((javaScript, idx) => (
+          {currentJavaScripts.map((javaScript, idx) => (
             <JavaScriptCard key={idx} {...javaScript} />
           ))}
         </div>
+
+        {/* Pagination Controls */}
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          handleNextPage={handleNextPage}
+          handlePrevPage={handlePrevPage}
+        />
       </article>
     </div>
   );
